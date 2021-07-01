ddac197bb078f3e8cb4b72494bedddaaaa74b0c1
Arthur bernardo <arthurutschgg@gmail.com>
2021-07-01 17:27:03 -0300

    finishing the component tela1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { Header } from '../components/Header'
-
+import { Tela1 } from '../components/tela1'
 export default function Home() {
   return (
+    <>
    <Header />
+   <Tela1 />
+   </>
   )
 }
